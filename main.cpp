8fbde1ab65d8cd3977541795cc14e5b679a2ce4f
Sibing <1915563868@qq.com>
2021-10-03 07:44:28 +0800

    Remove redundant include

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "memory_pool.h"
 #include "tree_node.h"
 #include "record.h"
-#include "memory_pool.cpp"
-#include "b_plus_tree.cpp"
-#include "record.cpp"
+// #include "memory_pool.cpp"
+// #include "b_plus_tree.cpp"
+// #include "record.cpp"
 
 #include <iostream>
 #include <fstream>
