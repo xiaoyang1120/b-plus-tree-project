c99cb682192c7bf332313f4625274f21d5f20234
Sibing <1915563868@qq.com>
2021-10-03 15:51:03 +0800

    format main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#pragma comment(linker, "/STACK:1073741824")
+//#pragma comment(linker, "/STACK:102400000,102400000")
 #include "b_plus_tree.h"
 #include "memory_pool.h"
 #include "tree_node.h"
@@ -76,96 +78,28 @@ int main()
         file.close();
     }
 
-    tree.search(500, 500);
-
-    cout << "Parameter n: " << tree.getMaxKeys() << endl;
-    cout << "Number of nodes: " << tree.getNumOfNodes() << endl;
-    cout << "Height: " << tree.getHeight() << endl;
-    cout << "Content of root: " << endl;
-    // for (int i = 0; i < tree.getRoot()->getNumOfKeys(); i++) {
-    //     cout << tree.getRoot()->getKey(i) << " ";
-    // }
-    tree.displayNode(tree.getRoot());
     cout << endl;
-    cout << "Content of first child node of root: " << endl;
-
-    // for (int i = 0; i < tree.getRoot()->getPointer(0)->getNumOfKeys(); i++) {
-    //     cout << tree.getRoot()->getPointer(0)->getKey(i) << " ";
-    // }
-    tree.displayNode(tree.getRoot()->getPointer(0));
-
-    cout << endl;
-
 
     cout << "Block Size: " << BLOCKSIZE << endl;
     cout << "Record Number: " << recordNum << endl;
+    
+    
+    cout<<"Experiment 1"<<endl;
     cout << "Number of Blocks: " << disk.getNumAllocated() << endl;
     cout << "Total Disk Size Used: " << disk.getTotalBlockSizeUsed() << "B" << endl;
-//    BPlusTree bptree;
-//     // int numbers[12] = {1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42};
-//     bptree.insert(1);
-//     bptree.insert(4);
-//     bptree.insert(7);
-//     bptree.insert(10);
-//     bptree.insert(17);
-//     bptree.insert(21);
-//     bptree.insert(31);
-//     bptree.insert(25);
-//     bptree.insert(19);
-//     bptree.insert(20);
-//     bptree.insert(28);
-//     bptree.insert(42);
-//     cout << "root: " << bptree.getRoot()->getKey(0) << endl;
-//    bptree.display(bptree.getRoot());
-//    int numU = 0;
-//    int numD = 0;
-//    bptree.remove(4, numD, numU);
-//    bptree.remove(1, numD, numU);
-//    bptree.remove(7, numD, numU);
-//    cout<<"num updates: "<<numU<<endl;
-//    cout<<"num delete/merge: "<<numD<<endl;
-//    cout << "root: " << bptree.getRoot()->getKey(0) << endl;
-//    bptree.display(bptree.getRoot());
-//    bptree.getFirstLeaf();
-
-    // int select = 0;
-    // cout<<"Select Block Size:"<<endl;
-    // do{
-    //     cout<<"1. 100B"<<endl;
-    //     cout<<"2. 500B"<<endl;
-    //     cin>>select;
-    // }
-    // while(select!=1 && select!=2);
-
-    //read data
-
-    // ifstream infile("data.tsv");
-    // string a, b, c;
-    // while (infile >> a >> b >> c)
-    // {
-    //     // process tconst, avgRating and numVotes
-    //     // cout<<a<<endl;
-    //     // create record
-	// // create a data block if there no data block or current data block is full
-	// // insert record into data block
-    // }
-
-
-    cout<<"Experiment 1"<<endl;
-    cout<<"Number of blocks: "<<endl;
-    cout<<"Size of database: "<<endl;
     cout<<endl;
 
     //create b+ tree
     //insert data into b+ tree
 
     cout<<"Experiment 2"<<endl;
-    cout<<"N = "<<endl;
-    cout<<"Number of blocks: "<<endl;
-    cout<<"Maximum of keys in a block: "<<endl;
-    cout<<"Maximum of pointers in a block: "<<endl;
-    cout<<"Height of B+ tree: "<<endl;
-    cout<<"First child node: "<<endl;
+    cout << "Parameter n: " << tree.getMaxKeys() << endl;
+    cout << "Number of nodes: " << tree.getNumOfNodes() << endl;
+    cout << "Height: " << tree.getHeight() << endl;
+    cout << "Content of root: " << endl;
+    tree.displayNode(tree.getRoot());
+    cout << "Content of first child node of root: " << endl;
+    tree.displayNode(tree.getRoot()->getPointer(0));
     cout<<"tconst: "<<endl;
     cout<<"Average Rating: "<<endl;
     cout<<"Number of Votes: "<<endl;
@@ -173,8 +107,9 @@ int main()
 
     //retrieve movies with votes = 500
     cout<<"Experiment 3"<<endl;
-    cout<<"Number of index nodes accessed: "<<endl;
-    cout<<"Content of index nodes: "<<endl;
+    tree.search(500, 500);
+    // cout<<"Number of index nodes accessed: "<<endl;
+    // cout<<"Content of index nodes: "<<endl;
     cout<<"Number of data blocks accessed: "<<endl;
     cout<<"Content of data blocks: "<<endl;
     cout<<"Average rating: "<<endl;
@@ -183,8 +118,9 @@ int main()
     //retrieve movies with votes between 30,000
     //and 40,000
     cout<<"Experiment 4"<<endl;
-    cout<<"Number of index nodes accessed: "<<endl;
-    cout<<"Content of index nodes: "<<endl;
+    tree.search(30000, 40000);
+    // cout<<"Number of index nodes accessed: "<<endl;
+    // cout<<"Content of index nodes: "<<endl;
     cout<<"Number of data blocks accessed: "<<endl;
     cout<<"Content of data blocks: "<<endl;
     cout<<"Average rating: "<<endl;
